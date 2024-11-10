b65c188d8275c8dc508e6f68769c2960318a1b28
William Santana <williamandrade1058@gmail.com>
2024-11-10 09:41:59 -0300

    modif description

--- src/page/partials/About/index.tsx
+++ src/page/partials/About/index.tsx
@@ -7,7 +7,7 @@ function About() {
         <section className={style.main_container}>
             <h3 className={style.title}>Sobre mim</h3>
             <p className={style.description}>
-            Profissional de TI com 9 meses de experiência em automação de processos (RPA), em transição para o desenvolvimento web back-end. Possuo uma base sólida em Python e Java, e estou focado em aprimorar habilidades em Java e Spring Boot para desenvolver soluções web escaláveis e de alta performance.
+            Profissional de TI com 9 meses de experiência em automação de processos (RPA), em transição para o desenvolvimento web back-end. Tenho conhecimentos em Python e Java e estou focado em aprimorar habilidades em Java e Spring Boot para desenvolver soluções web escaláveis e de alta performance.
             </p>
         </section>
 
